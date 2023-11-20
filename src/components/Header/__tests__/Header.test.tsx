3e3e1558e9a5adf995a50bfd92a86005a0a8c3e4
Manoela Coelho Silva <manoelacs62@gmail.com>
2023-11-19 21:25:04 -0300

    commit inicial

---
+++ src/components/Header/__tests__/Header.test.tsx
@@ -0,0 +1,44 @@
+import { screen } from '@testing-library/react'
+
+import Header from '..'
+import { renderizaComProvider } from '../../../utils/tests'
+
+describe('Testes para o componente header', () => {
+  test('Deve renderizar corretamente', () => {
+    renderizaComProvider(<Header />)
+    expect(screen.getByText('EBAC Games')).toBeInTheDocument()
+  })
+
+  test('Deve renderizar com 2 itens no carrinho', () => {
+    renderizaComProvider(<Header />, {
+      preloadedState: {
+        carrinho: {
+          itens: [
+            {
+              id: 1,
+              categoria: 'RPG',
+              imagem: '',
+              plataformas: ['Windows'],
+              preco: 150.9,
+              precoAntigo: 199.9,
+              titulo: 'Elden Ring'
+            },
+            {
+              id: 2,
+              categoria: 'RPG',
+              imagem: '',
+              plataformas: ['Windows', 'PS5'],
+              preco: 199.9,
+              precoAntigo: 279.9,
+              titulo: 'Hogwarts Legacy'
+            }
+          ]
+        }
+      }
+    })
+
+    expect(screen.getByTestId('quantidade-carrinho').innerHTML).toContain(
+      '2 itens'
+    )
+  })
+})
